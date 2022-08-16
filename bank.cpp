f19977fece3d004d2ab8a2018b9e9888dc08e23e
kamcio420 <kamcio420@gmail.com>
2022-08-16 22:47:09 +0200

    mainMenu, empty funcs

--- bank.cpp
+++ bank.cpp
@@ -1,7 +1,25 @@
 ﻿#include <iostream>
 
+void mainMenu();
+void Register();
+void Login();
+
 int main()
-{
+{      
     std::cout << "Hello World!\n";
 }
 
+void mainMenu()
+{
+    using std::cout;
+    char choice = '\0';
+    cout << "RLG Bank\n";
+    cout << "If you are new , register - 1\n";
+    cout << "Already have an account? login - 2\n";
+    while(choice != '1' || choice != '2')
+        std::cin >> choice;
+    if (choice == 1)
+        Register();
+    if (choice == 2)
+        Login();
+}
